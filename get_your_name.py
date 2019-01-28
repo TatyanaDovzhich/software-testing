568269df97bd8e48be68d8999c401a585856a312
Tatyana Dovzhich <dovzhich.tatyana@gmail.com>
2019-01-28 23:00:29 +0200

    Homework_Git_25.01.2019

---
+++ get_your_name.py
@@ -0,0 +1,6 @@
+def get_your_name(lastname, firstname, secondname):
+    my_name = lastname, firstname, secondname
+    return (my_name)
+
+
+print get_your_name('Dovzhich', 'Tatyana', 'Alecksandrovna')
